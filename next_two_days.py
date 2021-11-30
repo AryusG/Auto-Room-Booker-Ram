549f441300febab02dae9e3414afbdf50639f3e4
AryusG <verdyg24@gmail.com>
2021-11-30 11:22:42 +0000

    Going to remove self.month from get_day_month_year()

--- next_two_days.py
+++ next_two_days.py
@@ -15,25 +15,30 @@ class NewDateTwoDaysAhead():
         today_month = self.today_month
         today_year = self.today_year
 
+        print(today_day)
+        print(today_month)
+        print(today_year)
         total_month_days = monthrange(today_year, today_month)[1]
         day_difference = total_month_days - today_day #to check if today_date needs to be reset back to 1 or 2 for the new month 
 
         if today_month != 12 and day_difference == 1:
-            print("30")
+            print("29")
             today_day = 1
             self.today_month += 1
 
         elif today_month != 12 and day_difference == 0:
-            print("31")
+            print("Not Dec, day difference 0")
             today_day = 2
             self.today_month += 1
 
         if today_month == 12 and day_difference == 1:
+            print("December and difference of 1")
             today_day = 1
             today_month = 1
             self.today_year += 1
 
         elif today_month == 12 and day_difference == 0:
+            print("December and difference of 0")
             today_day = 2
             today_month = 1
             self.today_year += 1
@@ -60,5 +65,5 @@ class NewDateTwoDaysAhead():
         return current_month_string
 
 date = NewDateTwoDaysAhead()
-print(date.dt_today)
+# TODO TwoDaysAhead() is giving me 2/11 instead of 2/12
 print(date.get_day_month_year())
