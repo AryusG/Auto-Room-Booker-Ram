9515e84244e75416bdca4894561d785c36908c62
AryusG <verdyg24@gmail.com>
2021-11-30 11:37:43 +0000

    Fixed Date Problem

--- next_two_days.py
+++ next_two_days.py
@@ -1,5 +1,13 @@
 import datetime
 from calendar import monthrange
+from logging import raiseExceptions
+
+
+class DateTooLargeError(Exception):
+    '''Raised when day number is more than month day total'''
+    def __init__(self, message="Input date more than month total of days") -> None:
+        super().__init__(message)
+
 
 class NewDateTwoDaysAhead():
     def __init__(self) -> None:
@@ -15,35 +23,33 @@ class NewDateTwoDaysAhead():
         today_month = self.today_month
         today_year = self.today_year
 
-        print(today_day)
-        print(today_month)
-        print(today_year)
         total_month_days = monthrange(today_year, today_month)[1]
         day_difference = total_month_days - today_day #to check if today_date needs to be reset back to 1 or 2 for the new month 
 
-        if today_month != 12 and day_difference == 1:
+        if day_difference < 0:
+            raise DateTooLargeError
+
+        elif today_month != 12 and day_difference == 1:
             print("29")
             today_day = 1
-            self.today_month += 1
+            today_month += 1
 
         elif today_month != 12 and day_difference == 0:
             print("Not Dec, day difference 0")
             today_day = 2
-            self.today_month += 1
+            today_month += 1
 
-        if today_month == 12 and day_difference == 1:
-            print("December and difference of 1")
+        elif today_month == 12 and day_difference == 1:
             today_day = 1
             today_month = 1
-            self.today_year += 1
+            today_year += 1
 
         elif today_month == 12 and day_difference == 0:
-            print("December and difference of 0")
             today_day = 2
             today_month = 1
-            self.today_year += 1
+            today_year += 1
 
-        if day_difference > 1:
+        elif day_difference > 1:
             today_day += 2
 
         date_of_next_two = [today_day, today_month, today_year]
@@ -64,6 +70,3 @@ class NewDateTwoDaysAhead():
 
         return current_month_string
 
-date = NewDateTwoDaysAhead()
-# TODO TwoDaysAhead() is giving me 2/11 instead of 2/12
-print(date.get_day_month_year())
