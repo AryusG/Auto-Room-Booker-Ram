590407ace019615cfa5f4893bfda1971c6fd3e92
AryusG <verdyg24@gmail.com>
2021-11-30 11:55:22 +0000

    v0.2-reorganising-def-functions

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import time
+from datetime import datetime
 import os
 
 
@@ -16,27 +17,6 @@ class AutoRoomBooker():
         self.driver.maximize_window()
         self.two_days_ahead = NewDateTwoDaysAhead()
 
-
-    def login(self):
-        load_dotenv()
-        ram_webpage = "https://ram.asimut.net"
-        user_name = os.getenv("LOGIN")
-        password = os.getenv("PASS")
-        
-        self.driver.get(ram_webpage)
-
-        login_xpath = '//*[@id="login-username-2606"]'
-        pass_xpath = '//*[@id="login-password-2606"]'
-        button_login_xpath = '//*[@id="2606"]/button'
-
-        login_element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, login_xpath)))
-
-        time.sleep(1)
-
-        login_element.send_keys(user_name)
-        self.driver.find_element_by_xpath(pass_xpath).send_keys(password)
-        self.driver.find_element_by_xpath(button_login_xpath).click()
-    
 
     def pick_two_days_ahead(self):
         day_month_year_list = self.two_days_ahead.get_day_month_year()
@@ -88,6 +68,7 @@ class AutoRoomBooker():
             except (NoSuchElementException, StaleElementReferenceException):
                 print("day_a_tag errors")
 
+
     def pick_two_hours_ahead(self, current_time):
         time_list = current_time.split(":") 
         hours = int(time_list[0])
@@ -104,8 +85,38 @@ class AutoRoomBooker():
 
         return f"{hours_str}:{mins_str}"
 
+    
+    def get_current_time(self):
+        now = datetime.now()
+        current_time = now.strftime("%H:%M")
+        
+        return current_time
 
-    def book_general_practice_room(self, room_name_str, start_time):
+
+    def login(self):
+        load_dotenv()
+        ram_webpage = "https://ram.asimut.net"
+        user_name = os.getenv("LOGIN")
+        password = os.getenv("PASS")
+        
+        self.driver.get(ram_webpage)
+
+        login_xpath = '//*[@id="login-username-2606"]'
+        pass_xpath = '//*[@id="login-password-2606"]'
+        button_login_xpath = '//*[@id="2606"]/button'
+
+        login_element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, login_xpath)))
+
+        time.sleep(1)
+
+        login_element.send_keys(user_name)
+        self.driver.find_element_by_xpath(pass_xpath).send_keys(password)
+        self.driver.find_element_by_xpath(button_login_xpath).click()
+    
+
+    def book_general_practice_room(self, room_name_str):
+        start_time = self.get_current_time()
+
         g_practice_room_xpath = '//*[@id="left-column"]/h2[1]/a'
         try:
             g_practice_room_element = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, g_practice_room_xpath)))
@@ -123,6 +134,7 @@ class AutoRoomBooker():
                 room_name_element = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, name_xpath)))
                 print(room_name_element)
                 room_name_element.click()
+                print(f"YAY WE CLICKED {room_name_str}")
                 name_room_is_clicked = True
             except (ElementNotInteractableException, StaleElementReferenceException):    
                 print('name_room_stale_element_exception')
@@ -146,8 +158,9 @@ class AutoRoomBooker():
         end_time_element.send_keys(self.pick_two_hours_ahead(start_time))  
 
 bot = AutoRoomBooker()
+print(bot.get_current_time())
 bot.login()
-bot.book_general_practice_room('CK24 (General Practice Room)', "16:00")
+bot.book_general_practice_room('CK21 (General Practice Room)')
 
 
 
